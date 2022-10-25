a777e85e694d74eb85da6481b1814be2eaf38d61
Femnir81 <inmydefence81@yahoo.it>
2022-10-25 18:56:08 +0200

    Initial commit

---
+++ ts/script.ts
@@ -0,0 +1,40 @@
+class Account {
+    balanceInit: number = 0;
+    account: string;
+    constructor(account: string) {
+        this.account = account
+    }
+    
+    deposit(money: number) {
+        this.balanceInit = this.balanceInit + money;
+        console.log(`Il bilancio di ${this.account} è di ${this.balanceInit}`);
+    }
+
+    withdraw(money: number) {
+        this.balanceInit = this.balanceInit - money;
+        console.log(`Il bilancio di ${this.account} è di ${this.balanceInit}`);
+    }    
+}
+
+class MotherAccount extends Account {
+    addInterest() {
+        this.balanceInit = Math.floor(this.balanceInit * 1.1);
+        console.log(`Il bilancio di ${this.account} è di ${this.balanceInit}`);
+    }
+}
+
+class SonAccount extends Account { 
+}
+
+let mother1 = new MotherAccount('Sarah')
+let figlio1 = new SonAccount('Saro')
+
+mother1.deposit(600);
+mother1.withdraw(200);
+mother1.addInterest();
+
+figlio1.deposit(100);
+figlio1.withdraw(30);
+
+console.log(`Il bilancio di ${mother1.account} è di ${mother1.balanceInit} e Il bilancio di ${figlio1.account} è di ${figlio1.balanceInit}`)
+
